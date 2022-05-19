241735f909224f5884933b3c9e2f705de2a2a37f
zhangxue9 <zhangxue9@midea.com>
2022-05-19 13:48:35 +0800

    method reference

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JDK8新特性 研究学习使用---持续更新中
+# DK8新特性 研究学习使用
 
 Java 8 是目前最常用的 JDK 版本，在增强代码可读性、简化代码方面，做了很多优化，
 如 Lambda、Stream 、ParallelStream、Optional、新日期时间类型等。
@@ -17,7 +17,9 @@ https://blog.csdn.net/zx03070723/article/details/115735117
 
 *其实未使用 @FunctionalInterfaces注解的接口未必就不是函数式接口，一个接口是不是函数式接口的条件只有一条，即接口中只有一个抽象方法的接口（Object类中的方法不算，如hashcode方法）。*
 
-使用lambda表达式来实现函数式接口 , JDK8中内置函数式接口包括:  
+- 使用lambda表达式来实现函数式接口 
+
+  JDK8中内置函数式接口包括:  
 
 | 接口名       | 类                | 方法               | 方法描述             |
 | ------------ | ----------------- | ------------------ | -------------------- |
@@ -27,5 +29,13 @@ https://blog.csdn.net/zx03070723/article/details/115735117
 | 消费接口     | Consumer<T>       | void accept(T t);  | 有入参，无返回值     |
 | 断言接口     | Predicate<T>      | boolean test(T t); | 有入参，返回布尔类型 |
 
+- 函数式编程还可以使用双冒号:: 方法引用的方式  
+  结合Lambda表达式让编写的代码变得更加精简
 
+| 种类          | 语法                  | lambda表达式                     |
+| ------------- | --------------------- | -------------------------------- |
+| 静态方法引入  | ClassName::methodName | (args)->类名.static_method(args) |
+| 对象方法引入  | Class::method         | (instance)->instance.method()    |
+| 实例方法引入  | Instance::methodName  | (args)->对象实例.method(args)    |
+| 构造函数yinru | Class::new            | (args)->new 类名(args)           |
 
---
+++ src/main/java/com/example/jdk8demo/functionalprogramming/MethodReferenceExample.java
@@ -0,0 +1,54 @@
+package com.example.jdk8demo.functionalprogramming;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
+
+/**
+ * @description 函数式编程-方法引用 JSK8新特性之一
+ * 方法引入可以结合Lambda表达式让编写的代码变得更加精简
+ * 方法引用的4种写法,分别为 构造函数引入、静态方法引入、实例方法引入,对象方法引入
+ * @AUTHOR zhangxue9
+ * @DATE 2022/5/19 11:08
+ */
+@Slf4j
+public class MethodReferenceExample {
+    public static void main(String[] args) {
+        //User::new  构造函数引用
+        Supplier<User> userSupplier = User::new;
+        User user = userSupplier.get();
+        user.setName("Simon");
+        log.info("构造函数引用 name={} ",user.getName());
+        //实例方法引入
+        Supplier<String>  nameSupplier = user::getName;
+        log.info("实例方法引入 name={}",nameSupplier.get());
+        //静态方法引入
+        Supplier<Integer> getRandomNum =MethodReferenceExample::getRandomNum;
+        log.info("静态方法引入 random={}",getRandomNum.get());
+        //对象方法引入
+        MethodReferenceExample methodReferenceExample = new MethodReferenceExample();
+        Supplier<Integer>  integerSupplier= methodReferenceExample::getRandom;
+        log.info("非静态方法引入 random={}",integerSupplier.get());
+    }
+
+    public int getRandom(){
+        return ThreadLocalRandom.current().nextInt(100);
+    }
+
+    static int getRandomNum(){
+        return ThreadLocalRandom.current().nextInt(100);
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class User {
+        String name;
+        Integer age;
+    }
+}
